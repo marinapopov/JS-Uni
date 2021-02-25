cfc32e30bd8dcfd008b92bb3c42e9a6fee8204ca
marinapopov <54959447+marinapopov@users.noreply.github.com>
2021-02-25 13:25:26 +0200

    Update main.js

--- 2 - Instructiuni ciclice si repetitive/main.js
+++ 2 - Instructiuni ciclice si repetitive/main.js
@@ -45,19 +45,90 @@
 // console.log(testArr);
 
 
-//Ex2
+//Ex3
 
-const styles = ["Jazz", "Blues"];
-console.log(styles);
+// const styles = ["Jazz", "Blues"];
+// console.log(styles);
 
-const addElem = styles.push("Rock N Roll");
-console.log(styles);
+// const addElem = styles.push("Rock N Roll");
+// console.log(styles);
 
-const removed = styles.splice(1, 1, "Classic");
-console.log(styles);
+// const removed = styles.splice(1, 1, "Classic");
+// console.log(styles);
 
-styles.shift();
-console.log(styles);
+// styles.shift();
+// console.log(styles);
 
-styles.unshift("Rap", "Raggy");
-console.log(styles);
+// styles.unshift("Rap", "Raggy");
+// console.log(styles);
+
+//Ex4
+// function sumInput() {
+
+//     let arr = [];
+
+//     while (true) {
+
+//         let value = prompt("Enter a number to Sum");
+
+//         if (value === "" || value === null || !isFinite(value)) break;
+
+//         arr.push(+value);
+//     }
+
+//     let sum = 0;
+//     for (let number of arr) {
+//         sum += number;
+//     }
+//     return sum;
+// }
+
+// alert(sumInput());
+
+//Ex6
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const newArr = array => {
+//     const newGenerated = array;
+//     newGenerated.splice(4, 0, 0, 0, 0);
+//     return newGenerated;
+// }
+
+// console.log(newArr(arr));
+
+
+//Ex7
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const newArr = array => {
+//     return array.slice(2, 5)
+// }
+// console.log(newArr(arr));
+
+//Ex8
+// const colors = ["red", "green", "blue"];
+
+// const checkColor = color => {
+//    return colors.indexOf(color);
+// }
+// console.log(checkColor("green"));
+
+//Ex9
+// const arr = [1, "a", 2, null, 3, undefined, 4, {}, 5, 6, `word`, 7, 8, false, 9, 100];
+
+// let numbersOnly = (val) => {
+//     if (typeof(val) === 'number') {
+//         return val;
+//     }
+// }
+
+// let numbers = arr.filter(numbersOnly);
+// console.log(numbers);
+
+//Ex10
+
+const arr = [8, 7, 2, 2, 3, 4, 2, 5, 5, 6, 7, 8, 5, 9, 0, 1];
+
+const unsort = () => {
+    return [...new Set(arr)].sort((a, b) => a - b)
+}
+
+console.log(unsort());
