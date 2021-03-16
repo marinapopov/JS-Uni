89ac68f549422453597f7369d99e61f36f363f7b
marinapopov <54959447+marinapopov@users.noreply.github.com>
2021-03-17 00:16:32 +0200

    add ex6.3

--- Classes/main.js
+++ Classes/main.js
@@ -12,40 +12,68 @@
 // console.log(worker.surname);
 
 //Ex6.2
-class Person {
-    constructor(name, surname){
-        this._name = name;
-        this._surname = surname;
-    }
+// class Person {
+//     constructor(name, surname){
+//         this._name = name;
+//         this._surname = surname;
+//     }
 
-    getName(){
-        return this._name;
-    }
+//     getName(){
+//         return this._name;
+//     }
 
-    getSurname(){
-        return this._surname;
-    }
+//     getSurname(){
+//         return this._surname;
+//     }
 
-    setSurname(surname){
-        this._surname = surname;
-    }
-}
-
-const worker = new Person('Ivan', 'Ivanov');
-
-console.log(worker.getName());
-console.log(worker.getSurname());
-
-worker.setSurname('Petrov'); 
-
-console.log(worker.setSurname());
-console.log(worker.getSurname());
-
-//Ex6.4
-// class Validator {
-//     static isDomain() {
-
+//     setSurname(surname){
+//         this._surname = surname;
 //     }
 // }
 
-// let validator = new Validator();
+// const worker = new Person('Ivan', 'Ivanov');
+
+// console.log(worker.getName());
+// console.log(worker.getSurname());
+
+// worker.setSurname('Petrov'); 
+
+// console.log(worker.setSurname());
+// console.log(worker.getSurname());
+
+//Ex6.4
+class Validator {
+  
+    isEmail(email) {
+      let pattern = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+          return pattern.test(email);
+    }
+  
+    isDomain(domain) {
+      let pattern = /(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]/;
+          return pattern.test(domain);
+    }
+    
+    isDate(date) {
+      let pattern = /(0?[1-9]|[12]\d|30|31)[^\w\d\r\n:](0?[1-9]|1[0-2])[^\w\d\r\n:](\d{4}|\d{2})/;
+          return pattern.test(date);
+    }
+  
+    isPhone(phone) {
+      let pattern = /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/;
+          return pattern.test(phone);
+    }
+  
+  }
+  
+  var validator = new Validator();
+  
+  console.log(validator.isEmail('phphtml@mail.ru'));
+  console.log(validator.isDomain('businessbooks.com'));
+  console.log(validator.isDate('12.05.2020'));
+  console.log(validator.isPhone('1-415-444-1122'));
+  
+  
+  
+  
+  
