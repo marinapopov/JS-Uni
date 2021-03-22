07121c313ca587669595ab7f06e2fe2bba37741f
marinapopov <54959447+marinapopov@users.noreply.github.com>
2021-03-22 13:14:34 +0200

    add ex 7.15

--- DOM tree/main.js
+++ DOM tree/main.js
@@ -1,14 +1,25 @@
 //Ex7.1
-let changeBackground = (color) => {
-    document.body.style.background = color;
- }
+// let changeBackground = (color) => {
+//     document.body.style.background = color;
+//  }
  
- window.addEventListener("load",(color) => changeBackground('red') );
+//  window.addEventListener("load",(color) => changeBackground('red') );
 
- //Ex7.2
-setTimeout(() => document.body.style.background = 'green', 3000);
+//Ex7.2
+// setTimeout(() => document.body.style.background = 'green', 3000);
 
-// //Ex7.3
-let chgBgcolor = (color) => {
-  document.body.style.background = color;
+//Ex7.3
+// let chgBgcolor = (color) => {
+//   document.body.style.background = color;
+// }
+
+//Ex7.15
+let num = document.getElementById("num");
+num.onblur = num_blur;
+
+function num_blur() {
+    let input = this.value;
+    let number = +input;
+    if (number !== parseFloat(input) || !Number.isFinite(number))
+        this.value = "0";
 }
